99438e73314558272875ff17e94a599418f8ee4a
ViniciusEJunges <vinicius160501@gmail.com>
2023-03-01 23:24:32 -0300

    Login e tela inicial
    -Melhorias
    -Tela inicial
    -Validação usuário

---
+++ Formularios/logar.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "../banco/conexao.php";
+
+session_start();
+
+if (isset($_POST['usuario']) && isset($_POST['senha'])) {
+
+	$login = $_POST['usuario'];
+    $senha = $_POST['senha'];
+
+    $stmt = $conn->prepare("SELECT * FROM usuario where login=:login AND senha=:senha");
+    $stmt->bindParam(':login', $login);
+    $stmt->bindParam(':senha', $senha);
+    $stmt->execute();
+
+    if($stmt->rowCount() == 1) {
+        header("Location: index.php");
+        exit;
+    } else {
+    	$_SESSION["MensagemErroLogin"] = "Login ou senha incorretos.";
+    	header("Location: login.php");
+    }
+}
+?>
--- Formularios/login.php
+++ Formularios/login.php
@@ -1,53 +1,37 @@
-<?php
-
-require_once "../banco/config.php";
-
-session_start();
-
-if (isset($_POST['inputLogin']) && isset($_POST['inputPassword'])) {
-
-	$login = $_POST['inputLogin'];
-	$password = $_POST['inputPassword'];
-
-	if ($login == 'login' && $password == 'senha') {
-		$_SESSION['user'] = 'usuário'; // Define uma variável de sessão com o nome de usuário
-		header('Location: index.php'); // Redireciona para a página inicial após o login bem sucedido
-		exit();
-
-	} else {
-		$error_message = 'Email ou senha incorretos.'; // Define uma mensagem de erro se o login falhar
-	}
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-
-	<nav class="navbar fixed-top navbar-light bg-light" style ="background-color:#F8F8FF;">
-	    <a class="navbar-brand" href="#"><img src="../imagens/Logo_Escola_Caes_Guias_Keller.png" class ="logo"> </a>
-   </nav>
-
 	<title>Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
-<body>
-	<div class="container">
-		<form class="form-signin" method="post" action="login.php">
-			<h2 class="form-signin-heading">Login</h2>
 
-			<label for="inputLogin" class="sr-only">Email</label>
-			<input type="text" name="inputLogin" class="form-control" value="" placeholder="username">
-			<label for="inputPassword" class="sr-only">Senha</label>
-			<input type="password" id="inputPassword" name="inputPassword" class="form-control"/ placeholder="Senha" required>
-			<button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
-			<?php if (isset($error_message)) { ?>
-				<div class="alert alert-danger" role="alert"><?php echo $error_message; ?></div>
-			<?php } ?>
+<body>
+	<header>
+		<nav class="navbar fixed-top navbar-light bg-light" style ="background-color:#F8F8FF;">
+	    	<a class="navbar-brand" href="#"><img src="../imagens/Logo_Escola_Caes_Guias_Keller.png" class ="logo"> </a>
+		</nav>
+	</header>
+	
+	<main> 
+		<form method="post" action="logar.php">
+			<div class="container"> 
+				<h1 class="h3 mb-3 font-weight-normal"><b>Login</b></h1>
+				<label for="usuario" class="sr-only"></label>
+				<input type="text" name="usuario" class="form-control" value="" placeholder="Login">
+				<label for="senha" class="sr-only">Senha</label>
+				<input type="password" id="senha" name="senha" class="form-control" placeholder="Senha" required>
+				<a href="esqueci_senha.php">Esqueci minha senha</a>
+				<button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+				<?php session_start(); ?>
+				<?php if (isset($_SESSION["MensagemErroLogin"])) { ?>
+				<div class="alert alert-danger" role="alert"><?php echo $_SESSION["MensagemErroLogin"]; ?></div>
+				<?php } ?>
+				<?php session_destroy(); ?>
+			</div>
 		</form>
-	</div>
+	</main>
 </body>
 </html>
