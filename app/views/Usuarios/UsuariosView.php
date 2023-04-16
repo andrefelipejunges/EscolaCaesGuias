4dcb9fea1a96036540abcf5df809581b89b2bc1f
ViniciusEJunges <vinicius160501@gmail.com>
2023-04-16 17:21:23 -0300

    Ajuste visual CRUD cadastro

--- app/views/Usuarios/UsuariosView.php
+++ app/views/Usuarios/UsuariosView.php
@@ -17,21 +17,22 @@ if(isset($_POST['submit'])) {
         <form method="post" action ="<?php echo URL_BASE.'app/routes/routes.php?action=cadastrarUsuario';?>">
             <div class="container-cadastro"> 
                  <h1 class="h3 mb-3 font-weight-normal"><b>Dados</b></h1>
+                 <br>
             <div class="row"> 
                 <div class="col-md-4">
-                    <label for="login">Login</label>
+                   <b> <label for="login">Login:</label></b>
                     <input id="login" name="login" class="form-control" type="text" placeholder="Login..." required>
                 </div>
                 <div class="col-md-4">
-                    <label for="senha">Senha</label>
+                   <b> <label for="senha">Senha:</label> </b>
                     <input id="senha" name="senha" class="form-control" type="password" placeholder="Senha..." required>
                 </div>
                 <div class="col-md-5">
-                    <label for="email">E-mail</label>
+                    <b><label for="email">E-mail</label></b>
                     <input id="email" name="email" class="form-control" type="email" placeholder="E-mail..." required>
                 </div>
             </div>
-            <button class="btn btn-primary btn-lg mx-auto d-block" style="width: 150px;" type="submit" name="submit">Cadastrar</button>
+            <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">Cadastrar</button>
         </div>
         </form>
     </body>
